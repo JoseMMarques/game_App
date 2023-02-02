aa006b9f8b75e586c454aee5f4200faccfa8593c
jose <jmmarques@avalmancil.pt>
2023-02-02 11:50:36 +0000

    Formulario da participação em html

--- apps/game_features/forms.py
+++ apps/game_features/forms.py
@@ -5,6 +5,9 @@ from .models import Complaint
 
 # create a ModelForm
 class ComplaintAddForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ComplaintAddForm, self).__init__(*args, **kwargs)
+        self.fields['user'].disabled = True
 
     class Meta:
         model = Complaint
