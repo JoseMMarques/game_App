5887e6823f55dc60c3cc031a2422dd45251d5b4e
jose <jmmarques@avalmancil.pt>
2024-02-01 08:31:51 +0000

    commit para entrega do pc

--- apps/pias/views.py
+++ apps/pias/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from apps.pias.models import Pias
+
+@login_required(login_url='/contas/login/')
+def
